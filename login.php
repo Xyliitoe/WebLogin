d32401b69b1aaece7cce89d86958c5be58a1f2f7
Returnx_X <115673635+Xyliitoe@users.noreply.github.com>
2024-07-13 14:05:53 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+$servername = "localhost";
+$port = "*******";
+$username = "web***in";
+$password = "*********";
+$dbname = "web***in";
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $sql = "SELECT id, password FROM users WHERE username = '$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['id'];
+            header("Location: docxupload.html");
+            exit();
+        } else {
+            echo "Invalid password.";
+        }
+    } else {
+        echo "No user found.";
+    }
+}
+
+$conn->close();
+?>
